be9117e3dd8c73c1e66e8809037c7ff57723f33a
erickandreww <erick.andrew.moura@gmail.com>
2024-11-09 22:01:02 -0300

    "Project 01 - week 1 Completed"

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database')
-// const moongose = require('mongoose');
 const app = express();
 
 // body parser
